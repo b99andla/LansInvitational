0ded83e23ebbe823c79623f93a5e8f6ed16f5ca5
Andreas Lans <andreas.lans@mindius.se>
2017-08-03 14:33:45 +0200

    fixed scores

--- round/models.py
+++ round/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 # Create your models here.
 
@@ -37,6 +38,25 @@ class Score(models.Model):
         Team,
         on_delete=models.CASCADE
     )
+
+    @property
+    def total_strokes(self):
+        strokes = self.scoreline_set.aggregate(Sum('strokes'))
+        print(strokes['strokes__sum'])
+        if strokes['strokes__sum'] is not None:
+            return strokes['strokes__sum']
+        else:
+            return '-'
+
+    @property
+    def on_hole(self):
+        hole = self.scoreline_set.exclude(strokes__isnull=True).count()
+        if hole == 0:
+            return 'Har inte startat än'
+        if hole == 18:
+            return 'har gått klart'
+        else:
+            return hole + 1
 
     def __str__(self):
         return '{} score'.format(self.team)
--- round/views.py
+++ round/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Round, Score
+from django.template import loader
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This will show results.")
+    latest_round_list = Round.objects.order_by('name')
+    template = loader.get_template('round/index.html')
+    context = {'latest_round_list': latest_round_list, }
+
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, round_id):
+    score_list = Score.objects.filter(
+        team__round_id=round_id).order_by('team__name')
+    template = loader.get_template('round/detail.html')
+    context = {'score_list': score_list}
+    return HttpResponse(template.render(context, request))
